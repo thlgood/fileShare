1a452e3d8f4d5a2672c592e455ffe4d15c24e44f
thlgood <thlgood@yucoat.com>
2012-08-13 19:12:18 +0800

    clean

--- test.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-import os
-def showDir(abspath):
-    allFiles = os.listdir(abspath)
-    for one in xrange(len(allFiles)):
-        allFiles[one] = allFiles[one].encode('UTF-8')
-        print allFiles[one], '--->', type(allFiles[one])
-
-showDir('/home/tang')
